794043762aad493ef3014f52433da47ddd80f418
Meashyshy <camillefvrl@gmail.com>
2022-12-12 03:40:16 -0800

    Added argument to %d

--- pop.c
+++ pop.c
@@ -12,7 +12,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n");
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		free(stack);
 		exit(EXIT_FAILURE);
 	}
