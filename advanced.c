864ad0a1ce53c0bee8c5e43fe4670691b94bb140
Meashyshy <camillefvrl@gmail.com>
2022-12-14 06:06:44 -0800

    Fixed syntaxe error

--- advanced.c
+++ advanced.c
@@ -13,7 +13,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !((*stack)->next))
 	{
-		fprintf(stderr, L%d: can't sub, stack too short\n, line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 		return;
 	}
