bda4fb3f90c388c8d07552799f06d6e39d595724
Meashyshy <camillefvrl@gmail.com>
2022-12-14 06:04:20 -0800

    Added sub function

--- advanced.c
+++ advanced.c
@@ -17,3 +17,7 @@ void sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 		return;
 	}
+	sub = ((*stack)->next->n) - ((*stack->n));
+	pop(stack, line_number);
+	return;
+}      	
